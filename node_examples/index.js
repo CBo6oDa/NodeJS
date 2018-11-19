a0738b7258910a60cb08ea4a410d5daf0e5430c2
Chornogirskiy <Chornogirskiy@ukr.net>
2018-11-19 04:56:04 -0800

    Node HTTP Example 1

--- node_examples/index.js
+++ node_examples/index.js
@@ -1,3 +1,5 @@
+var rect = require('./rectangle');
+
 function solveRect(l,b) {
     console.log("Solving for rectangle with l = "
                 + l + " and b = " + b);
@@ -14,3 +16,8 @@ function solveRect(l,b) {
     });
     console.log("This statement after the call to rect()");
 };
+
+solveRect(2,4);
+solveRect(3,5);
+solveRect(0,5);
+solveRect(-3,5);
