3a050c4a530f255c8339a060e85acd7f663173bd
Chornogirskiy <Chornogirskiy@ukr.net>
2018-11-20 06:30:04 -0800

    Mongoose Part 2

--- node-mongoose/index.js
+++ node-mongoose/index.js
@@ -9,27 +9,24 @@ connect.then((db) => {
 
     console.log('Connected correctly to server');
 
-    var newDish = Dishes({
-        name: 'Uthappizza',
+    Dishes.create({
+        name: 'Uthapiza',
         description: 'test'
+    })
+    .then((dish) => {
+        console.log(dish);
+        
+        return Dishes.find({}).exec();
+    })
+    .then((dishes) => {
+        console.log(dishes);
+        return db.collection('dishes').drop();
+    })
+    .then(() => {
+        return db.close();        
+    })
+    .catch((err) => {
+        console.log(err);
     });
-
-    newDish.save()
-        .then((dish) => {
-            console.log(dish);
-
-            return Dishes.find({});
-        })
-        .then((dishes) => {
-            console.log(dishes);
-
-            return Dishes.remove({});
-        })
-        .then(() => {
-            return mongoose.connection.close();
-        })
-        .catch((err) => {
-            console.log(err);
-        });
 
 });
